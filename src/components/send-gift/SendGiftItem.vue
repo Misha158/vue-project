37ebc56d6c459d387d5b1c12db8b55aa862a04d5
mikhail-svekolnikov1 <mikhail@gifted.co>
2025-07-27 17:10:54 +0300

    Form with steps - created list of items

---
+++ src/components/send-gift/SendGiftItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex justify-between items-center gap-4 p-4 border rounded shadow-sm">
+    <div class="flex items-center gap-4 p-4">
+      <img :src="item.imageUrl" alt="Item image" class="w-16 h-16 object-cover rounded" />
+      <div class="flex flex-col">
+        <h2 class="font-semibold text-lg">{{ item.title }}</h2>
+        <p class="text-gray-500 text-sm">{{ item.subTitle }}</p>
+        <div class="flex flex-wrap gap-2 mt-2">
+          <span
+            v-for="tag in item.tags"
+            :key="tag"
+            class="bg-pink-100 text-pink-700 text-xs px-2 py-1 rounded-full"
+          >
+            #{{ tag }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <UiButton @click="$emit('edit', item)">Edit</UiButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import UiButton from '@/components/UiButton.vue';
+
+interface Item {
+  imageUrl: string;
+  title: string;
+  subTitle: string;
+  recipientIds: number[];
+  tags: string[];
+}
+
+defineProps<{
+  item: Item;
+}>();
+
+defineEmits<{
+  (e: 'edit', item: Item): void;
+}>();
+</script>
--- src/views/SendGiftView.vue
+++ src/views/SendGiftView.vue
@@ -1,7 +1,57 @@
 <template>
-  <div>SEND A GIFT</div>
+  <div>
+    <div>
+      <UiButton class="flex ms-auto mb-20">Create</UiButton>
+
+      <div class="grid gap-4">
+        <SendGiftItem v-for="(item, index) in items" :key="index" :item="item" @edit="handleEdit" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UiButton from '@/components/UiButton.vue';
+import { ref } from 'vue';
+import SendGiftItem from '@/components/send-gift/SendGiftItem.vue';
+
+interface Item {
+  imageUrl: string;
+  title: string;
+  subTitle: string;
+  recipientIds: number[];
+  tags: string[];
+}
+
+const defaultItems: Item[] = [
+  {
+    imageUrl: 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg',
+    title: 'Birthday Gift',
+    subTitle: 'For Anna’s Birthday',
+    recipientIds: [1, 2],
+    tags: ['Birthday', 'Special'],
+  },
+  {
+    imageUrl: 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg',
+    title: 'Anniversary Surprise',
+    subTitle: 'Celebrating 2 years',
+    recipientIds: [3],
+    tags: ['Love', 'Surprise'],
+  },
+  {
+    imageUrl: 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg',
+    title: 'Thank You Gift',
+    subTitle: 'Appreciation for help',
+    recipientIds: [],
+    tags: ['Thanks'],
+  },
+];
+
+const items = ref<Item[]>(defaultItems);
+
+const handleEdit = () => {
+  return;
+};
+</script>
 
 <style scoped></style>
