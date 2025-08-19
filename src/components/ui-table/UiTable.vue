5abec04e783a095fe81ba980bf34166c14d7c328
mikhail-svekolnikov1 <mikhail@gifted.co>
2025-08-19 12:37:06 +0300

    Save UiTable v2

---
+++ src/components/ui-table/UiTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ui-table-wrapper">
+    <table class="ui-table">
+      <UiTableHeader :columns="columns" />
+      <UiTableBody :data="data" :columns="columns" />
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UiTableHeader from '@/components/ui-table/UiTableHeader.vue';
+import type { Column } from '@/types/ui-table.ts';
+import UiTableBody from '@/components/ui-table/UiTableBody.vue';
+
+interface Props {
+  data: Array<Record<string, unknown>>;
+  columns: Column[];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.ui-table-wrapper {
+  width: 100%;
+  overflow-x: auto;
+}
+
+.ui-table {
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0;
+}
+</style>
